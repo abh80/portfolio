efd89fbe16a91465dd0264c43c9371a2d89058cc
abh80 <techixspotify@gmail.com>
2023-02-08 18:25:27 +0530

    sitemap update

--- src/pages/sitemap.xml.tsx
+++ src/pages/sitemap.xml.tsx
@@ -1,30 +1,36 @@
 import * as http from "http";
 
 function generateSiteMap() {
-    return `<?xml version="1.0" encoding="UTF-8"?>
+  return `<?xml version="1.0" encoding="UTF-8"?>
    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    
+   <url>
+    <loc>https://abh80.site</loc>
+    <lastmod>2023-02-08</lastmod>
+  </url>
    </urlset>
  `;
 }
 
 function SiteMap() {
-    // getServerSideProps will do the heavy lifting
+  // getServerSideProps will do the heavy lifting
 }
 
-export async function getServerSideProps({res}: { res: http.ServerResponse }) {
+export async function getServerSideProps({
+  res,
+}: {
+  res: http.ServerResponse;
+}) {
+  // We generate the XML sitemap with the posts data
+  const sitemap = generateSiteMap();
 
-    // We generate the XML sitemap with the posts data
-    const sitemap = generateSiteMap();
+  res.setHeader("Content-Type", "text/xml");
+  // we send the XML to the browser
+  res.write(sitemap);
+  res.end();
 
-    res.setHeader('Content-Type', 'text/xml');
-    // we send the XML to the browser
-    res.write(sitemap);
-    res.end();
-
-    return {
-        props: {},
-    };
+  return {
+    props: {},
+  };
 }
 
 export default SiteMap;

d037d00cd23cf85548228c54cb2a452e7790e6d7
abh80 <techixspotify@gmail.com>
2023-02-10 19:41:03 +0530

    refactor: change footer appearence

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,6 +1,8 @@
 import ViewItem from "@/components/ViewItem";
 import MetaResolver from "@/components/MetaResolver";
+import { Kanit } from "@next/font/google";
 
+const kanit = Kanit({ weight: ["700", "500", "400"], subsets: ["latin"] });
 export default function NotFound() {
   return (
     <>
@@ -9,7 +11,24 @@ export default function NotFound() {
         description="404 - Not Found"
       />
       <ViewItem>
-        <div>404</div>
+        <div className="mt-10 space-y-10">
+          <h1
+            className={
+              kanit.className + " text-xl font-bold text-center w-full"
+            }
+          >
+            404 - Not Found
+          </h1>
+
+          <h2
+            className={
+              kanit.className + " text-lg font-semibold text-center w-full"
+            }
+          >
+            You are definitely lost right?
+          </h2>
+          <div className="h-96" />
+        </div>
       </ViewItem>
     </>
   );

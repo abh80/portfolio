4c0639fdca6dd9d84a49741abda6335006f39b96
abh80 <techixspotify@gmail.com>
2023-02-14 11:01:10 +0530

    refactor: improve animations

--- src/components/ChildMenuItem.tsx
+++ src/components/ChildMenuItem.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Roboto } from "@next/font/google";
 import Link from "next/link";
-import { motion } from "framer-motion";
+import { motion, Variants } from "framer-motion";
 
 const roboto = Roboto({ subsets: ["latin"], weight: ["400", "500", "700"] });
 export default function ChildMenuItem({
@@ -13,27 +13,51 @@ export default function ChildMenuItem({
   active?: boolean;
   hash: string;
 }) {
+  const containerVariants: Variants = {
+    hover: {},
+    click: {},
+  };
+  const childVariants: Variants = {
+    hover: {
+      scale: 1.2,
+      transition: {
+        type: "spring",
+        stiffness: 400,
+        damping: 10,
+      },
+    },
+    click: {
+      scale: 0.9,
+    },
+  };
   return (
-    <Link
-      data-hash={hash}
-      shallow={true}
-      href={"#" + hash}
-      className={
-        "flex group w-full cursor-pointer gap-3 transition-all border-l border-slate-600 hover:border-slate-500 -ml-px " +
-        (active ? "border-slate-400" : "")
-      }
+    <motion.div
+      variants={containerVariants}
+      whileHover="hover"
+      whileTap="click"
     >
-      <span
-        className={[
-          "text-[1rem] font-semibold align-middle transition-all ml-5 capitalize",
-          active
-            ? "text-slate-200"
-            : "text-slate-500 group-hover:text-slate-300 group-hover:scale-[1.1] transform",
-          roboto.className,
-        ].join(" ")}
+      <Link
+        data-hash={hash}
+        shallow={true}
+        href={"#" + hash}
+        className={
+          "flex group w-full cursor-pointer gap-3 border-l border-slate-600 hover:border-slate-500 -ml-px " +
+          (active ? "border-slate-400" : "")
+        }
       >
-        {title}
-      </span>
-    </Link>
+        <motion.span
+          variants={active ? {} : childVariants}
+          className={[
+            "text-[1rem] font-semibold align-middle  ml-5 capitalize",
+            active
+              ? "text-slate-200"
+              : "text-slate-500 group-hover:text-slate-300",
+            roboto.className,
+          ].join(" ")}
+        >
+          {title}
+        </motion.span>
+      </Link>
+    </motion.div>
   );
 }
--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Roboto } from "@next/font/google";
 import Link from "next/link";
+import { motion, Variants } from "framer-motion";
 
 const roboto = Roboto({ subsets: ["latin"], weight: ["400", "500", "700"] });
 export default function MenuItem({
@@ -16,37 +17,58 @@ export default function MenuItem({
   bgColor?: string;
   url: string;
 }) {
+  const containerVariants: Variants = {
+    hover: {},
+    click: {},
+  };
+  const childVariants: Variants = {
+    hover: {
+      scale: 1.2,
+      transition: {
+        type: "spring",
+        stiffness: 400,
+        damping: 10,
+      },
+    },
+    click: {
+      scale: 0.9,
+    },
+  };
   return (
-    <Link
-      href={url}
-      className={
-        "flex group w-full cursor-pointer gap-2 transition-all items-center " +
-        (active ? "" : "transform hover:scale-[1.1]")
-      }
+    <motion.div
+      variants={containerVariants}
+      whileHover="hover"
+      whileTap="click"
     >
-      {icon ? (
-        <div
+      <Link
+        href={url}
+        className={"flex group w-full cursor-pointer gap-3 items-center"}
+      >
+        {icon ? (
+          <div
+            className={[
+              `align-middle transition-all bg-slate-400/50 px-1 rounded-md items-center`,
+              active ? bgColor : "",
+              active ? "text-white" : "text-slate-300",
+              `group-hover:bg-blue-400`,
+            ].join(" ")}
+          >
+            <i className="fa-solid fa-house fa-sm"></i>
+          </div>
+        ) : null}
+        <motion.span
+          variants={active ? {} : childVariants}
           className={[
-            `align-middle transition-all bg-slate-400/50 px-1 rounded-md items-center`,
-            active ? bgColor : "",
-            active ? "text-white" : "text-slate-300",
-            `group-hover:bg-blue-400`,
+            "text-[0.9rem] font-semibold align-middle transition-color",
+            active
+              ? "text-slate-200"
+              : "text-slate-500 group-hover:text-slate-300",
+            roboto.className,
           ].join(" ")}
         >
-          <i className="fa-solid fa-house fa-sm"></i>
-        </div>
-      ) : null}
-      <span
-        className={[
-          "text-[0.9rem] font-semibold align-middle transition-all",
-          active
-            ? "text-slate-200"
-            : "text-slate-500 group-hover:text-slate-300",
-          roboto.className,
-        ].join(" ")}
-      >
-        {title}
-      </span>
-    </Link>
+          {title}
+        </motion.span>
+      </Link>
+    </motion.div>
   );
 }
